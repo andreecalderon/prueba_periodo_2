5bbdf3c0f4afb95341733d45eee872bb8b4d553a
andreecalderon <jacalderon@scl.edu.gt>
2024-05-28 09:34:19 -0600

    header

---
+++ header/header.js
@@ -0,0 +1,9 @@
+let header = document.querySelector(".header");
+
+header.innerHTML=`
+<footer>
+    <div class="blo_1">bloque izquierdo</div>
+    <div class="blo_2">header</div>
+    <div class="blo_3">bloque derecho</div>
+</footer>
+`
---
+++ section/section.js
@@ -0,0 +1,12 @@
+let section = document.querySelector(".section")
+
+section.innerHTML=`
+    <section class="section">
+    <div class="bloque1"></div>
+    <div class="bloque2"></div>
+    <div class="bloque3"></div>
+    <div class="bloque4"></div>
+    <div class="bloque5"></div>
+    </section>
+
+`
